5a90d7cb37bf80601f20c9511883362f8a0200e1
Kieran Singh <kieransingh27@gmail.com>
2022-07-15 13:57:52 +1000

    Replaced image with stock image, removed bug where qty could be zero

--- src/data/product-data.js
+++ src/data/product-data.js
@@ -1,59 +1,60 @@
+import Image from "../images/bike-img.jpg"
 const data = [
   {
     name: 'Orginal Bike',
     id:'1',
     price: 10,
-    stock: Math.floor(Math.random() * 10),
-    img: 'https://cdn.shopify.com/s/files/1/0226/4114/7984/products/reid-cycles-australia-rapid-road-bike-black-s-385_700x.png?v=1620957346'
+    stock: Math.floor(Math.random() * 10) + 1,
+    img: "https://thumbs.dreamstime.com/b/mountain-sports-bike-isolated-white-background-145823668.jpg"
   },
   {
     name: 'City Bike',
     id:'2',
     price: 15,
-    stock: Math.floor(Math.random() * 10),
-    img: 'https://cdn.shopify.com/s/files/1/0226/4114/7984/products/reid-cycles-australia-rapid-road-bike-black-s-385_700x.png?v=1620957346'
+    stock: Math.floor(Math.random() * 10) + 1,
+    img: "https://thumbs.dreamstime.com/b/mountain-sports-bike-isolated-white-background-145823668.jpg"
   },
   {
     name: 'Trekking Bike',
     id:'3',
     price: 20,
-    stock: Math.floor(Math.random() * 10),
-    img: 'https://cdn.shopify.com/s/files/1/0226/4114/7984/products/reid-cycles-australia-rapid-road-bike-black-s-385_700x.png?v=1620957346'
+    stock: Math.floor(Math.random() * 10) + 1,
+    img: "https://thumbs.dreamstime.com/b/mountain-sports-bike-isolated-white-background-145823668.jpg"
   },
   {
     name: 'Offroad Bike',
     id:'4',
     price: 25,
-    stock: Math.floor(Math.random() * 10),
-    img: 'https://cdn.shopify.com/s/files/1/0226/4114/7984/products/reid-cycles-australia-rapid-road-bike-black-s-385_700x.png?v=1620957346'
+    stock: Math.floor(Math.random() * 10) + 1,
+    img: "https://thumbs.dreamstime.com/b/mountain-sports-bike-isolated-white-background-145823668.jpg"
   },
   {
     name: "Express Bike",
     id:'5',
     price: 30,
-    stock: Math.floor(Math.random() * 10),
-    img: 'https://cdn.shopify.com/s/files/1/0226/4114/7984/products/reid-cycles-australia-rapid-road-bike-black-s-385_700x.png?v=1620957346'
+    stock: Math.floor(Math.random() * 10) + 1,
+    img: "https://thumbs.dreamstime.com/b/mountain-sports-bike-isolated-white-background-145823668.jpg"
   },
   {
     name: "Rapid Bike",
     id:'6',
     price: 40,
-    stock: Math.floor(Math.random() * 10),
-    img: 'https://cdn.shopify.com/s/files/1/0226/4114/7984/products/reid-cycles-australia-rapid-road-bike-black-s-385_700x.png?v=1620957346'
+    stock: Math.floor(Math.random() * 10) + 1,
+    img: "https://thumbs.dreamstime.com/b/mountain-sports-bike-isolated-white-background-145823668.jpg"
   },
   {
     name: "Mono 2.0 Bike",
     id:'7',
     price: 50,
-    stock: Math.floor(Math.random() * 10),
-    img: 'https://cdn.shopify.com/s/files/1/0226/4114/7984/products/reid-cycles-australia-rapid-road-bike-black-s-385_700x.png?v=1620957346'
+    stock: Math.floor(Math.random() * 10) + 1,
+    img: "https://thumbs.dreamstime.com/b/mountain-sports-bike-isolated-white-background-145823668.jpg"
   },
   {
     name: "Wind 3.0 Bike",
     id:'8',
     price: 60,
-    stock: Math.floor(Math.random() * 10),
-    img: 'https://cdn.shopify.com/s/files/1/0226/4114/7984/products/reid-cycles-australia-rapid-road-bike-black-s-385_700x.png?v=1620957346'
+    stock: Math.floor(Math.random() * 10) + 1,
+    img: "https://thumbs.dreamstime.com/b/mountain-sports-bike-isolated-white-background-145823668.jpg"
   }
 ]
 
